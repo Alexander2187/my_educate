b261496cbafafe09f8cf02753fbc85b2289515f9
Alexander2187 <alexworkavto@gmail.com>
2024-10-13 12:32:16 +0300

    Add files via upload

---
+++ module_9/m_9_4/module_9_4.py
@@ -0,0 +1,38 @@
+"""
+Задача "Функциональное разнообразие"
+"""
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+var = list(map(lambda x, y: x == y, first, second))
+print(var)
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as text_io:
+            for item in data_set:
+                text_io.write(str(item) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball.words)
+print(first_ball())
+print(first_ball())
+print(first_ball())
