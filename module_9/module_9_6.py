d731c6773e6bde2e18113831296e74508fe982c8
Alexander2187 <alexworkavto@gmail.com>
2024-10-15 22:02:48 +0300

    Add files via upload

---
+++ module_9/module_9_6.py
@@ -0,0 +1,26 @@
+def all_variants(text):
+    is_letter = False
+    if len(text) > 0:
+        is_letter = True
+    while is_letter:
+        for index in range(len(text)):
+            """ На каждой итеррации цикла 
+            возвращаем срез, который короче предыдущего 
+            на одну букву с конца строки. 'abcd', 'abc', 'ab', 'a'.
+            """
+            yield (text[0:len(text)-index])
+        if len(text) > 0:
+            is_letter = True
+            """ Убираем одну букву с начала строки.  'abcd' -> 'bcd' """
+            text = text[1:]
+        else:
+            is_letter = False
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+a = all_variants("Watermelon")
+for i in a:
+    print(i)
