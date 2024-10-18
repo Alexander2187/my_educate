accd8a2b8df7e20f6cd31f332a4f367c14883d97
Alexander2187 <alexworkavto@gmail.com>
2024-10-18 21:27:04 +0300

    Add files via upload

---
+++ module_9/module_9_7.py
@@ -0,0 +1,31 @@
+def is_prime(f):
+    def wrapper(*args):
+        var = f(*args)
+        if var % 2 == 0:
+            print("Составное")
+            return var
+        d = 3
+        while d * d <= var and var % d != 0:
+            d += 2
+        if d * d > var:
+            print("Простое")
+            return var
+        else:
+            print("Составное")
+            return var
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+if __name__ == '__main__':
+    print("------------------>  if __name__ == '__main__'  <------------------")
+
+    print(sum_three(1, 4, 6))
+    print(sum_three(2, 4, 6))
+    print(sum_three(3, 4, 6))
+
