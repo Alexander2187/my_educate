2f7ad695a60deba45ce9cdb0222740c85f3af9f8
Alexander2187 <alexworkavto@gmail.com>
2024-12-15 15:12:39 +0300

    Add files via upload

--- module_12/m_12_1/module_12_1.py
+++ module_12/m_12_1/module_12_1.py
@@ -2,28 +2,31 @@
 "Простые Юнит-Тесты"
 Задача "Проверка на выносливость"
 """
-
-from unittest import TestCase
-from runner import Runner
+from unittest import TestCase, skipIf
+import lessons.module_12.m_12_1.runner as rn
 
 
 class RunnerTest(TestCase):
+    is_frozen = False
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
-        x = Runner("SomeName")
+        x = rn.Runner("SomeName")
         for i in range(10):
             x.walk()
         self.assertEqual(x.distance, 50)
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
-        x = Runner("SomeName")
+        x = rn.Runner("SomeName")
         for i in range(10):
             x.run()
         self.assertEqual(x.distance, 100)
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
-        x = Runner("Some_Walker")
-        y = Runner("Some_Runner")
+        x = rn.Runner("Some_Walker")
+        y = rn.Runner("Some_Runner")
         for i in range(10):
             x.walk()
             y.run()
